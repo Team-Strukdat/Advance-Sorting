72c648dea5bd29fb47dc9c1e0a7badbe45791123
Fathin Muhashibi Putra <fathinmp11@gmail.com>
2022-04-14 10:36:06 +0700

    insertionsortstl

---
+++ Soal No 1/InsertionSortStl/Modular/header.h
@@ -0,0 +1,6 @@
+
+// C++ program to implement insertion sort using STL.
+#include <bits/stdc++.h>
+
+void insertionSort(std::vector<int> &vec);
+void print(std::vector<int> vec);
---
+++ Soal No 1/InsertionSortStl/Modular/insertionSort.cpp
@@ -0,0 +1,16 @@
+#include "header.h"
+  
+// Function to sort the array
+void insertionSort(std::vector<int> &vec)
+{
+    for (auto it = vec.begin(); it != vec.end(); it++)
+    {        
+        // Searching the upper bound, i.e., first 
+        // element greater than *it from beginning
+        auto const insertion_point = 
+                std::upper_bound(vec.begin(), it, *it);
+          
+        // Shifting the unsorted part
+        std::rotate(insertion_point, it, it+1);        
+    }
+}
